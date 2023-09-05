98a1e13333a189bfaf26f414d1c638ffad169f1d
saad-bin-farooq <i200555@nu.edu.pk>
2023-09-05 20:31:44 +0500

    updated substract function

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def multiply(x,y):
     
     return x * y
 
-def sub(x):
-    return x - 3
+def sub(x,y):
+    return x - y
   
 def add(x,y):
     return x+y
--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from main import square, sub
 def test_square():
     assert 4 == square(2)
 def test_sub():
-    assert 1 == sub(4)
+    assert 1 == sub(4,3)
