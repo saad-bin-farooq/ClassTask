fee416da9355139a7d9c275f5a3f01443d48d038
BilalBadar <youremail@example.com>
2023-09-05 23:22:35 +0500

    Adding subtract func 2 and test case

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def multiply(x,y):
 
 def sub(x,y):
     return x - y
-  
+def sub2(x):
+    return x - 10
+
 def add(x,y):
     return x+y
 
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from main import square, sub
+from main import square, sub, sub_2
 
 def test_square():
     assert 4 == square(2)
 def test_sub():
     assert 1 == sub(4,3)
+def test_sub2():
+    assert 0 == sub_2(10)
